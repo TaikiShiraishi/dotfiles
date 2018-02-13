bb32074430f6f369f8ef17dd23cdeba1946ef1d8
TaikiShiraishi <tiki.t.s0118@gmail.com>
2018-02-13 18:41:56 +0900

    Remove unused line

--- config.fish
+++ config.fish
@@ -9,7 +9,6 @@ set -x NDENV_ROOT "/Users/tshiraishi/.anyenv/envs/ndenv"
 set -x PATH $PATH "/Users/tshiraishi/.anyenv/envs/ndenv/bin"
 set -x PATH $NDENV_ROOT/shims $PATH
 # rbenv
-rbenv init - | source
 set -x RBENV_ROOT "/Users/tshiraishi/.anyenv/envs/rbenv"
 set -x PATH $PATH "/Users/tshiraishi/.anyenv/envs/rbenv/bin"
 set -x  PATH $RBENV_ROOT/shims $PATH
