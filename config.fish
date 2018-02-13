d588a1c593147e0fd290a2245028b3ac3fd546f2
TaikiShiraishi <tiki.t.s0118@gmail.com>
2018-02-13 15:05:17 +0900

    Add anyenv setting

--- config.fish
+++ config.fish
@@ -1,3 +1,15 @@
 function fish_user_key_bindings
   bind \cr 'peco_select_history (commandline -b)'
 end
+
+# anyenv
+set -x PATH $HOME/.anyenv/bin $PATH
+# ndenv
+set -x NDENV_ROOT "/Users/tshiraishi/.anyenv/envs/ndenv"
+set -x PATH $PATH "/Users/tshiraishi/.anyenv/envs/ndenv/bin"
+set -x PATH $NDENV_ROOT/shims $PATH
+# rbenv
+rbenv init - | source
+set -x RBENV_ROOT "/Users/tshiraishi/.anyenv/envs/rbenv"
+set -x PATH $PATH "/Users/tshiraishi/.anyenv/envs/rbenv/bin"
+set -x  PATH $RBENV_ROOT/shims $PATH
