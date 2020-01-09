60af04dff85ae231af356b6fcf6892e247bc8e0e
Taiki Shiraishi <tiki.t.s0118@gmail.com>
2020-01-09 14:01:43 +0900

    add Brewfile and deleted unused files

---
+++ Brewfile
@@ -0,0 +1,87 @@
+tap "cloudfoundry/tap"
+tap "heroku/brew"
+tap "homebrew/bundle"
+tap "homebrew/cask"
+tap "homebrew/core"
+tap "homebrew/services"
+
+# brew
+brew "anyenv"
+brew "autoconf"
+brew "cloudfoundry/tap/cf-cli"
+brew "colordiff"
+brew "composer"
+brew "ffmpeg"
+brew "gcc"
+brew "gdbm"
+brew "git"
+brew "heroku/brew/heroku"
+brew "imagemagick"
+brew "jq"
+brew "libidn"
+brew "libxml2"
+brew "libxslt"
+brew "libyaml"
+brew "mas"
+brew "nkf"
+brew "pandoc"
+brew "pkg-config"
+brew "postgresql"
+brew "protobuf"
+brew "putty"
+brew "readline"
+brew "reattach-to-user-namespace"
+brew "redis", restart_service: true
+brew "tig"
+brew "tmux"
+brew "tree"
+brew "unbound", link: false
+brew "watchman"
+brew "wget"
+brew "yarn"
+brew "zsh"
+brew "zsh-autosuggestions"
+brew "zsh-completions"
+brew "zsh-syntax-highlighting"
+
+
+# cask
+cask "adguard"
+cask "adobe-creative-cloud"
+cask "alfred"
+cask "appcleaner"
+cask "atom"
+cask "basictex"
+cask "brave-browser"
+cask "clipy"
+cask "cursorsense"
+cask "docker"
+cask "dropbox"
+cask "firefox"
+cask "gitify"
+cask "google-chrome"
+cask "google-drive-file-stream"
+cask "hyper"
+cask "karabiner-elements"
+cask "macvim"
+cask "microsoft-office"
+cask "notion"
+cask "pomotodo"
+cask "scroll-reverser"
+cask "sketch"
+cask "spectacle"
+cask "spotify"
+cask "tunnelblick"
+cask "typora"
+cask "virtualbox"
+cask "visual-studio-code"
+cask "webstorm"
+
+
+# mas
+mas "Bitwarden", id: 1352778147
+mas "CotEditor", id: 1024640650
+mas "Keynote", id: 409183694
+mas "Slack", id: 803453959
+mas "The Unarchiver", id: 425424353
+mas "Xcode", id: 497799835
--- setup.sh
+++ setup.sh
@@ -8,104 +8,14 @@ function command_exists {
 #
 read -sp "Your Password:" pass;
 
-#
-# Mac App Store apps install
-#
-if ! command_exists mas ; then
-  echo "---------- Mac App Store apps ----------"
-  brew install mas
-  # Xcode
-  mas install 497799835
-  echo "---------- END ----------"
-fi
 
 #
 # Install zsh
 #
-if ! command_exists zsh ; then
+if command_exists zsh ; then
   echo "---------- zsh ----------"
-  brew install zsh zsh-autosuggestions zsh-completions zsh-syntax-highlighting colordiff
   which -a zsh
   echo $pass | sudo -S -- sh -c 'echo '/usr/local/bin/zsh' >> /etc/shells'
   chsh -s /usr/local/bin/zsh
   echo "---------- END ----------"
-fi
-
-
-#
-# Vim
-#
-if ! command_exists vim ; then
-  echo "---------- Vim ----------"
-  brew install vim --with-override-system-vi
-  echo "---------- END ----------"
-fi
-
-#
-# Install Ricty Diminished
-#
-echo "---------- Ricty Diminished ----------"
-git clone https://github.com/mzyy94/RictyDiminished-for-Powerline.git
-cp RictyDiminished-for-Powerline/Ricty*.ttf ~/Library/Fonts/
-cp RictyDiminished-for-Powerline/vim-powerline-fontpatched/Ricty*.ttf ~/Library/Fonts/
-fc-cache -f
-echo "---------- END -----------"
-
-#
-# Install Yarn
-#
-if ! command_exists yarn ; then
-  echo "---------- Yarn ----------"
-  brew install yarn
-  echo "----------- END ------------"
-fi
-
-#
-# Install anyenv
-#
-if ! command_exists anyenv ; then
-  echo "---------- Anyenv ----------"
-  brew install anyenv
-  echo "---------- END ----------"
-fi
-
-
-#
-# Install wget
-#
-if ! command_exists wget ; then
-  echo "---------- wget ----------"
-  brew install wget
-  wget --version
-  echo "---------- END ----------"
-fi
-
-#
-# Install jq
-#
-if ! command_exists jq ; then
-  echo "---------- jq ----------"
-  brew install jq
-  jq --veriosn
-  echo "---------- END ----------"
-fi
-
-#
-# Install tree
-#
-if ! command_exists tree ; then
-  echo "---------- tree ----------"
-  brew install tree
-  tree --version
-  echo "---------- END ----------"
-fi
-
-#
-# Install tmux
-#
-if ! command_exists tmux ; then
-  echo "---------- tmux ----------"
-  brew install tmux
-  tmux --version
-  echo "--------- END ----------"
 fi
